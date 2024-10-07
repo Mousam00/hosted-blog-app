2705e08d75975cf5c4f5a123f79efb491c4f22b3
mousam <mousampandey61@gmail.com>
2024-10-07 23:14:53 +0530

    build file

--- myapp/settings.py
+++ myapp/settings.py
@@ -132,6 +132,6 @@ LOGIN_REDIRECT_URL='/'
 LOGOUT_REDIRECT_URL='/'
 
 
-import os 
-STATICFILES_DIRS=os.path.join(BASE_DIR,"static")
-STATIC_ROOT=os.path.join(BASE_DIR,"staticfiles_build","static")
+# import os 
+# STATICFILES_DIRS=os.path.join(BASE_DIR,"static")
+# STATIC_ROOT=os.path.join(BASE_DIR,"staticfiles_build","static")
