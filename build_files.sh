7a153423a905097bd0cc93adaa10aa7e1dbcf407
mousam <mousampandey61@gmail.com>
2024-10-08 21:12:54 +0530

    fix

--- build_files.sh
+++ build_files.sh
@@ -2,7 +2,7 @@
 uv venv
 source .venv/bin/activate  
 uv pip install -r requirements.txt
-# python3.12 manage.py collectstatic
+python3.12 manage.py collectstatic
 
 # [optional] Start the application here 
 python manage.py runserver
