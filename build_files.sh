7fb45523a8ffb42fa60cc44ef6aa66749b866519
mousam <mousampandey61@gmail.com>
2024-10-08 21:08:07 +0530

    fix

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,8 @@
 # build_files.sh
-pip3 install -r requirements.txt
-python3.12 manage.py collectstatic
+uv venv
+source .venv/bin/activate  
+uv pip install -r requirements.txt
+# python3.12 manage.py collectstatic
 
 # [optional] Start the application here 
 # python manage.py runserver
