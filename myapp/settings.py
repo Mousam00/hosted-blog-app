15f9d97f4a9b6f9500d8e1472f4527cfa6b12fbb
mousam <mousampandey61@gmail.com>
2024-10-08 20:43:25 +0530

    fix

--- myapp/settings.py
+++ myapp/settings.py
@@ -132,6 +132,3 @@ LOGIN_REDIRECT_URL='/'
 LOGOUT_REDIRECT_URL='/'
 
 
-# import os 
-# STATICFILES_DIRS=os.path.join(BASE_DIR,"static")
-# STATIC_ROOT=os.path.join(BASE_DIR,"staticfiles_build","static")
