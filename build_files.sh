c12682b53a08704584277d7d963f313f1eeb1db0
mousam <mousampandey61@gmail.com>
2024-10-08 21:05:16 +0530

    fix

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,6 @@
 # build_files.sh
 pip3 install -r requirements.txt
-python3.9 manage.py collectstatic
+python3.12 manage.py collectstatic
 
 # [optional] Start the application here 
 # python manage.py runserver
